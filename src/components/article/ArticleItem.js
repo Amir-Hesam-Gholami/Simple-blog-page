e272b9261c552affcaf08e3084c5a618b9b06cd1
Amir Hesam Gholami <140734166+Amir-Hesam-Gholami@users.noreply.github.com>
2024-08-22 13:05:02 +0330

    feat : article psge & article data

--- src/components/article/ArticleItem.js
+++ src/components/article/ArticleItem.js
@@ -2,6 +2,7 @@ import Card from "react-bootstrap/Card";
 import { BiTimeFive } from "react-icons/bi";
 import { TiArrowLeftThick } from "react-icons/ti";
 import "./ArticleItem.css";
+import { Link } from "react-router-dom";
 function ArticleItem(props) {
   return (
     <Card>
@@ -9,10 +10,12 @@ function ArticleItem(props) {
       <Card.Body>
         <Card.Title className="py-2">{props.title}</Card.Title>
         <Card.Text>{props.desc}</Card.Text>
-        <span className="read-more">
-          <span>ادامه مقاله</span>
-          <TiArrowLeftThick size="25px" />
-        </span>
+        <Link to={`/article/${props.id}`}>
+          <span className="read-more">
+            <span>ادامه مقاله</span>
+            <TiArrowLeftThick size="25px" />
+          </span>
+        </Link>
       </Card.Body>
       <Card.Footer className="d-flex justify-content-between align-items-center py-3">
         <span>نویسنده : {props.writter}</span>
